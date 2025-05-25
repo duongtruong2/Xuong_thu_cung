c8f1e74c38fffaf3d1864ce328a166df34402dc1
duongtruong <truongdz2903@gmail.com>
2025-05-25 12:15:58 +0700

    tai khoan

--- xuong-thu-cung/xuong-thu-cung/admin/controllers/AdminTaiKhoanController.php
+++ xuong-thu-cung/xuong-thu-cung/admin/controllers/AdminTaiKhoanController.php
@@ -1,4 +1,5 @@
 <?php
+
 class AdminTaiKhoanController
 {
     public $modelTaiKhoan;
@@ -14,46 +15,51 @@ class AdminTaiKhoanController
 
     public function danhSachQuanTri()
     {
-        $listTaiKhoan = $this->modelTaiKhoan->getAllTaiKhoan();
-        require_once './views/taikhoan/listTaiKhoan.php';
+        $listQuanTri = $this->modelTaiKhoan->getAllTaiKhoan(1);
+
+        require_once './views/taikhoan/quantri/listQuanTri.php';
     }
 
     public function formAddQuanTri()
     {
-       require_once './views/taikhoan/quantri/addQuanTri.php';
+        require_once './views/taikhoan/quantri/addQuanTri.php';
 
-       deleteSessionError();
+        deleteSessionError();
     }
 
-    public function postAddQuanTri(){
-        if ($_SERVER['REQUEST_METHOD']=='POST'){
-            $ho_ten =$_POST['ho_ten'];
+
+    public function postAddQuanTri()
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $ho_ten = $_POST['ho_ten'];
             $email = $_POST['email'];
 
-            $errors=[];
-            if (empty($ho_ten)){
-                $errors['ho_ten'] = 'Tên không được để trống';
+            $errors = [];
+            if (empty($ho_ten)) {
+                $errors['ho_ten'] = 'Tên  không được để trống';
             }
-            if (empty($email)){
-                $errors['email'] = 'Email không được để trống';
+            if (empty($email)) {
+                $errors['email'] = 'Email  không được để trống';
             }
-            $_SESSION['errors'] = $errors;
-            if (empty($errors)){
-                $password =password_hash('123@123ab', PASSWORD_BCRYPT);
+            $_SESSION['error'] = $errors;
 
-                $chuc_vu_id = 1;
+            if (empty($errors)) {
+
+
+                $password = password_hash('123@123ab',PASSWORD_BCRYPT);
+                $chuc_vu_id = 1; // Chức vụ quản trị
+                // var_dump($password);die;
                 $this->modelTaiKhoan->insertTaiKhoan($ho_ten, $email, $password, $chuc_vu_id);
-
+                
                 header("Location: " . BASE_URL_ADMIN . '?act=list-tai-khoan-quan-tri');
                 exit();
-            }else{
-                $_SESSION['flash']=true;
-
+            } else {
+                $_SESSION['flash'] = true;
+               
                 header("Location: " . BASE_URL_ADMIN . '?act=form-them-quan-tri');
                 exit();
             }
         }
     }
-    
-
 }
--- xuong-thu-cung/xuong-thu-cung/admin/models/AdminTaiKhoan.php
+++ xuong-thu-cung/xuong-thu-cung/admin/models/AdminTaiKhoan.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 class AdminTaiKhoan
 {
@@ -9,67 +9,39 @@ class AdminTaiKhoan
         $this->conn = connectDB();
     }
 
-    public function getAllTaiKhoan()
-    {
+    public function getAllTaiKhoan($chuc_vu_id){
         try {
-            $sql = 'SELECT * FROM tai_khoans WHERE chuc_vu_id = :chuc_vu_id';;
+            $sql = 'SELECT * FROM tai_khoans WHERE chuc_vu_id = :chuc_vu_id';
+
             $stmt = $this->conn->prepare($sql);
-            $stmt->execute([':chuc_vu_id' => $chuc_vu_id]);
+
+            $stmt->execute([':chuc_vu_id'=>$chuc_vu_id]);
+
             return $stmt->fetchAll();
         } catch (Exception $e) {
-            echo "Lỗi: " . $e->getMessage();
+            echo "lỗi" . $e->getMessage();
         }
     }
 
     public function insertTaiKhoan($ho_ten, $email, $password, $chuc_vu_id){
-        try{
-             $sql = 'INSERT INTO tai_khoans (ho_ten, email, mat_khau, chuc_vu_id)
+        try {
+            $sql = 'INSERT INTO tai_khoans (ho_ten, email, mat_khau, chuc_vu_id)
                     VALUES (:ho_ten, :email, :password, :chuc_vu_id)';
+
             $stmt = $this->conn->prepare($sql);
+
             $stmt->execute([
                 ':ho_ten' => $ho_ten,
                 ':email' => $email,
-                ':password' => $password,               
-                ':chuc_vu_id' => $chuc_vu_id
+                ':password' => $password,
+                ':chuc_vu_id' => $chuc_vu_id,
             ]);
-            return true;
-        }catch (Exception $e) {
-            echo "Lỗi: " . $e->getMessage();
-            return false;
-        }
-    }
 
-    public function getDetailTaiKhoan($id)
-    {
-        try {
-            $sql = 'SELECT * FROM tai_khoans WHERE id = :id';
-            $stmt = $this->conn->prepare($sql);
-            $stmt->execute([':id' => $id]);
-            return $stmt->fetch();
-        } catch (Exception $e) {
-            echo "Lỗi: " . $e->getMessage();
-        }
-    }
-
-
-    public function updateTaiKhoan($id, $ho_ten, $email, $so_dien_thoai, $trang_thai)
-    {
-        try {
-            $sql = 'UPDATE tai_khoans SET ho_ten = :ho_ten, email = :email, mat_khau = :password, chuc_vu_id = :chuc_vu_id WHERE id = :id';
-            $stmt = $this->conn->prepare($sql);
-          $stmt->execute([
-                ':ho_ten' => $ho_ten,
-                ':email' => $email,
-                ':so_dien_thoai' => $so_dien_thoai,
-                ':trang_thai' => $trang_thai,
-                ':id' => $id
-            ]);
             return true;
         } catch (Exception $e) {
-            echo "Lỗi: " . $e->getMessage();
-            return false;
+            echo "lỗi" . $e->getMessage();
+            
         }
     }
-
-    
+  
 }
